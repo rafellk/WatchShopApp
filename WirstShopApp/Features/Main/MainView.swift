a6e6bd12546b5cfedf8bbeaebbe0613c58ffd3da
Rafael Lucena <rafellk@gmail.com>
2021-09-20 12:18:07 -0300

    Introduce WatchListItem component

--- WirstShopApp/Features/Main/MainView.swift
+++ WirstShopApp/Features/Main/MainView.swift
@@ -36,6 +36,12 @@ struct MainView: View {
                     }
                     .padding(.horizontal, 30)
                     .padding(.bottom, 24)
+                    
+                    VStack(spacing: 24) {
+                        WatchListItem(imageName: "ormous", title: "ORMOUS", subtitle: "White, size L", price: "249")
+                        WatchListItem(imageName: "hisako", title: "HISAKO", subtitle: "Black, size M", price: "329")
+                    }
+                    .padding(.horizontal, 30)
                 }
             }
         }
@@ -50,5 +56,34 @@ struct ContentView_Previews: PreviewProvider {
             MainView()
                 .previewDevice("iPhone 8")
         }
+    }
+}
+
+struct WatchListItem: View {
+    
+    let imageName: String
+    let title: String
+    let subtitle: String
+    let price: String
+    
+    var body: some View {
+        HStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: 93, height: 93)
+                .padding(8)
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .font(.custom(Fonts.metropolisMedium.rawValue, size: 17))
+                Text(subtitle)
+                    .font(.custom(Fonts.metropolisBook.rawValue, size: 13))
+            }
+            .padding(.leading, 12)
+            Spacer()
+            Text("$\(price)")
+                .font(.custom(Fonts.metropolisLight.rawValue, size: 24))
+                .padding(.trailing, 8)
+        }
+        .background(Color("watch_item_background"))
     }
 }
