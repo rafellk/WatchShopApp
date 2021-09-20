17bad62dd78116676312829e336c65348f0d5067
Rafael Lucena <rafellk@gmail.com>
2021-09-20 11:00:47 -0300

    Introduce the CustomNavigationView component

---
+++ WirstShopApp/Features/Common/Views/CustomNavigationView.swift
@@ -0,0 +1,30 @@
+//
+//  CustomNavigationView.swift
+//  WirstShopApp
+//
+//  Created by Rafael Lucena on 20/09/21.
+//
+
+import SwiftUI
+
+struct CustomNavigationView: View {
+    
+    var title: String
+    
+    var body: some View {
+        HStack {
+            Image("menu")
+            Spacer()
+            Text(title)
+            Spacer()
+            Image("search")
+        }
+        .padding(.horizontal, 30)
+    }
+}
+
+struct CustomNavigationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomNavigationView(title: "Watches")
+    }
+}
