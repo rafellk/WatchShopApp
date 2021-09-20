df5bebe2a3f57564abe05a49ca9fea4180a61d5c
Rafael Lucena <rafellk@gmail.com>
2021-09-20 10:52:55 -0300

    App structure

---
+++ WirstShopApp/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  WirstShopApp
+//
+//  Created by Rafael Lucena on 20/09/21.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Testing")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+            .previewDevice("iPhone 12 Pro")
+    }
+}
---
+++ WirstShopApp/WirstShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WirstShopAppApp.swift
+//  WirstShopApp
+//
+//  Created by Rafael Lucena on 20/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WirstShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
