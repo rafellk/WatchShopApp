b064c17142068fa05135bff84daa19ce6af6de03
Rafael Lucena <rafellk@gmail.com>
2021-09-20 11:52:39 -0300

    Introduce watches carousel

--- WirstShopApp/Features/Main/MainView.swift
+++ WirstShopApp/Features/Main/MainView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        Text("Testing")
+        GeometryReader { reader in
+            VStack() {
+                CustomNavigationView(title: "Watches")
+                    .padding(.bottom, 30)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    LazyHStack {
+                        Image("watch1")
+                            .frame(width: reader.size.width * 0.75)
+                            .padding(.leading, 30)
+                        Image("watch2")
+                            .frame(width: reader.size.width * 0.75)
+                            .padding(.leading, 30)
+                    }
+                    .frame(height: (reader.size.width * 0.75) * 1.14)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView()
-            .previewDevice("iPhone 12 Pro")
+        Group {
+            MainView()
+                .previewDevice("iPhone 12 Pro")
+            MainView()
+                .previewDevice("iPhone 8")
+        }
     }
 }
